02d1a62546359a749899982215b03a0ce2dcd460
TeryKing <tery_x3@hotmail.com>
2022-07-27 21:13:00 -0500

    score storing correctly working in console. need initals,score, and highscore displayed

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,4 @@
+//Every element used in index.html 
 const start = document.getElementById("start");
 const quiz = document.getElementById("quiz");
 const question = document.getElementById("question");
@@ -10,8 +11,9 @@ const timeGauge = document.getElementById("timeGauge");
 const scoreDiv = document.getElementById("scoreContainer");
 const retry = document.getElementById("retry");
 const save = document.getElementById("save");
+const rescoreDiv = document.getElementById("rescorecontainer");
 
-// create our questions
+// Creating the questions to a variable
 let questions = [
     {
         question : "What does HTML stand for?",
@@ -37,7 +39,7 @@ let questions = [
     }
 ];
 
-// create some variables
+//Some variables for the clock timer, setting the questions to a numeric value to run easier if statements, and the timer gauge bar
 let timerinterval = 5;
 const lastQuestion = questions.length - 1;
 let runningQuestion = 0;
@@ -48,7 +50,7 @@ const gaugeUnit = gaugeWidth / questionTime;
 let TIMER;
 let score = 0;
 
-// render a question
+//Renders the questions
 function renderQuestion(){
     let q = questions[runningQuestion];
     
@@ -58,11 +60,11 @@ function renderQuestion(){
     choiceC.innerHTML = q.choiceC;
 	choiceD.innerHTML = q.choiceD;
 }
-
+//Putting a eventlistener for when user clicks on Start
 start.addEventListener("click",startQuiz);
 
 
-// start quiz
+//Function starts the quiz and removes the starting block and adding a quiz block, calls the renderquestion function and rendercounter and puts the timer with intervals of 1 second
 function startQuiz(){
     start.style.display = "none";
     renderQuestion();
@@ -72,7 +74,7 @@ function startQuiz(){
 }
 
 
-// counter render
+//Function for counting the timer, if theres still time, then timer will tick down by 1 second and change the gauge accordingly.
 
 function renderCounter(){
     if(count > 0)
@@ -81,6 +83,7 @@ function renderCounter(){
         timeGauge.style.width = count * gaugeUnit + "px";
         count--;
     }
+    //If timer hits 0, then the timer will end and calls the scoreRender which ends the quiz and shows the final score.
 	else{
             clearInterval(TIMER);
             scoreRender();
@@ -88,28 +91,27 @@ function renderCounter(){
     }
 
 
-// checkAnwer
+//check function is for "onclick" in the index.html. This function will see whether the selected choice was the correct choice from the questions.
 
-function checkAnswer(answer){
+function check(answer){
     if( answer == questions[runningQuestion].correct)
 	{
-        // answer is correct
+        //If the answer is correct, then it will add points to the score
         score++;
-        // change progress color to green
-
+        //If the current question is not the last question, then it renders the next question. Using numeric values to generate which question is which.
 		if(runningQuestion < lastQuestion){
 			runningQuestion++;
 			renderQuestion();
 		}
 		else{
-			// end the quiz and show the score
+			//Otherwise if it is the last question, the timer is cleared and quiz ends.
 			clearInterval(TIMER);
 			scoreRender();
 		}
     }
 	else{
-        // answer is wrong
-
+        //If the selected choice is incorrect, then the if statement will follow as.
+        //IF the timer still has more than or equal to 5 seconds, then it will remove 5 seconds from the current timer.
 		if (count >= 5)
 		{
 			for(i=0; i<timerinterval; i++)
@@ -117,12 +119,14 @@ function checkAnswer(answer){
 				renderCounter();
 			}
 		}
+        //If timer is less than 5 seconds and the incorrect answer is chosen, then count will hit 0.
 		else
 		{
 			count = 0;
 
 		}
     }
+    //If count hits 0, then the quiz ends and shows the score
     if (count === 0)
 	{
 		clearInterval(TIMER);
@@ -131,29 +135,30 @@ function checkAnswer(answer){
   
 }
 
-// score render
+let scorePerCent="";
+//This function is for the final score of the quiz. It first changes the scorediv from none to a block to create a score screen.
 function scoreRender(){
     scoreDiv.style.display = "block";
-    // calculate the amount of question percent answered by the user
-    const scorePerCent = Math.round(100 * score/questions.length);
-    scoreDiv.innerHTML += "<p>"+ scorePerCent +"%</p>";
+    //This will function a score percentage based on how many user inputed a correct answer by how many questions there are. The it just displays a percentage of the final score.
+    scorePerCent = Math.round(100 * score/questions.length);
+    rescoreDiv.innerHTML = "<p>"+ scorePerCent +"%</p>";
 }
-
+//The following function is just a restart function, this will set the question back to the first question, set the count timer back to 100 seconds, the current score of correct answers to 0.
+//then rehides the score screen, then calls the StartQuiz function to restart the quiz.
 function restart()
 {
-    console.log(retry);
 	 runningQuestion = 0;
 	 count = 100;
 	 score = 0;
 	 scoreDiv.style.display = "none";
-	 startQuiz();
+     startQuiz();
 }
 
 retry.addEventListener("click",restart);
 
-save.addEventListener("click", Userscore);
+ function userscore()
+ {
+ 	localStorage.setItem("User", scorePerCent);
+ }
 
-function Userscore{
-	localStorage.setItem("High Score", score);
-}
-
+ save.addEventListener("click", userscore);
